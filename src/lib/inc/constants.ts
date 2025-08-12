8b3b74739803ab8c31a7afc8f66b5e9f1c2a142f
sunwoo <nonomiz@gifi.co.jp>
2025-08-12 10:41:17 +0900

    port change

--- src/lib/inc/constants.ts
+++ src/lib/inc/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://110.233.64.184:3000"
-export const API_ADMIN_URL = "http://110.233.64.184:3001"
+export const API_URL = "http://110.233.64.184:3010"
+export const API_ADMIN_URL = "http://110.233.64.184:3011"
