28ff174af357a565b9b57baa6202f9e63bde5971
sunwoo <nonomiz@gifi.co.jp>
2025-08-12 10:46:41 +0900

    http -> https

--- src/lib/inc/constants.ts
+++ src/lib/inc/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://110.233.64.184:3010"
-export const API_ADMIN_URL = "http://110.233.64.184:3011"
+export const API_URL = "https://110.233.64.184:3010"
+export const API_ADMIN_URL = "https://110.233.64.184:3011"
