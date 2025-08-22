0892e4adab826bcaa420edebf7bee9240071ab98
sunwoo <nonomiz@gifi.co.jp>
2025-08-22 11:11:37 +0900

    헤더 타이틀 위치 조정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,10 +51,10 @@ export default function RootLayout({
             <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
               <div className="container flex h-14 items-center">
                 <div className="mr-4 flex">
-                  <Link className="mr-6 flex items-center space-x-2" href="/">
-                    <span>予約</span>
-                  </Link>
-                  <nav className="flex items-center space-x-6 text-sm font-medium">
+                  <nav className="flex items-center space-x-6 text-sm font-medium p-3">
+                    <Link className="transition-colors hover:text-foreground/80 text-foreground/60" href="/">
+                      <span>予約</span>
+                    </Link>
                     <Link
                       href="/store/reservations"
                       className="transition-colors hover:text-foreground/80 text-foreground/60"
